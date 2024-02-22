ac93c6708c6e2d30355bede5cf28c3fb0ec8d3d8
Patsy Sprung <patsysprung@patsys-mbp-2.home>
2024-02-22 13:03:52 +0000

    error handling GET/api/users

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,7 +4,6 @@ exports.getAllUsers = (req, res, ext) =>{
     usersModel.selectAllUsers().then((users) => {
         res.status(200).send({users})
     }).catch((err) =>{
-        console.log(err, '<-- err in users controller')
         next(err)
     })
 }
