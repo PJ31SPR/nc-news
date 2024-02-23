eaa57a724f84d308e3716b1e52b9dec9f77c2d16
Patsy Sprung <patsysprung@patsys-mbp-2.home>
2024-02-23 10:38:28 +0000

    hosting edits

--- listen.js
+++ listen.js
@@ -1,9 +1,8 @@
 const app = require('./app/app.js');
 
-app.listen(9090,(err) =>{
-    if(err)
-         console.log(err);
-    else
-        console.log('listening on port 9090')
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT})`))
+
+
 
